2dad12ade3ec646f03079776d1f3359910846555
AndreaVk <vico.ac1996@gmail.com>
2021-12-16 20:42:50 +0100

    header

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Header from './components/macro/Header.vue';
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/img/style/global.scss';
+</style>
---
+++ src/components/macro/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png" alt="">
+    </header>
+</template>
+
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/img/style/variables.scss';
+    header{
+        width: 100%;
+        height: 70px;
+        background-color: $mainColor;
+        display: flex;
+        align-items: center;
+        img{
+            height: 70px;
+            padding: 10px;
+        }
+    }
+</style>
